de89b5465233750dde7e8728d935e216225837aa
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-09-23 00:48:17 +0600

    navbar added & pages added

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,3 +1,12 @@
 function openBlog() {
   window.location.href = "blog.html";
 }
+
+function openHistory() {
+  window.location.href = "history.html"
+}
+
+function openIndex() {
+  window.location.href = "index.html"
+
+}
