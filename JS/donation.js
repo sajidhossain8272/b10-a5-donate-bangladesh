449071f9444c222681b2d7695684e385d4b2ef2d
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-09-23 09:01:16 +0600

    donation function added

---
+++ JS/donation.js
@@ -0,0 +1,17 @@
+function processDonation(inputId) {
+  const donationAmount = parseFloat(document.getElementById(inputId).value);
+  const totalBalanceElement = document.getElementById('user-balance');
+  let totalBalance = parseFloat(totalBalanceElement.innerText);
+
+  if (donationAmount && donationAmount > 0) {
+    if (donationAmount <= totalBalance) {
+      totalBalance -= donationAmount;
+      totalBalanceElement.innerText = totalBalance.toFixed(2);
+      document.getElementById('my_modal_5').showModal();
+    } else {
+      alert("Insufficient balance.");
+    }
+  } else {
+    alert("Please enter a valid donation amount.");
+  }
+}
